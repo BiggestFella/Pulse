5c3db78ea31e4dda68ba600cd371c6afd126df37
BAKERSLCB <leoncreedbaker@gmail.com>
2026-06-11 19:43:21 +1000

    feat(builder): folder builder carries parentID for create-into-folder [BAK-27]

--- Pulse/Features/Builders/FolderBuilderModel.swift
+++ Pulse/Features/Builders/FolderBuilderModel.swift
@@ -6,18 +6,22 @@ import Observation
 final class FolderBuilderModel {
     var name: String = "New folder"
     var colorToken: FolderColor = .default
+    var parentID: Folder.ID? = nil
     var saveState: SaveState = .idle
 
     private let folderRepo: any FolderRepository
 
-    init(folders: any FolderRepository) { self.folderRepo = folders }
+    init(folders: any FolderRepository, parentID: Folder.ID? = nil) {
+        self.folderRepo = folders
+        self.parentID = parentID
+    }
 
     func select(color: FolderColor) { colorToken = color }
 
     func save() async {
         saveState = .saving
         do {
-            _ = try await folderRepo.createFolder(name: name, color: colorToken, parentID: nil)
+            _ = try await folderRepo.createFolder(name: name, color: colorToken, parentID: parentID)
             saveState = .saved
         } catch {
             saveState = .error("Couldn't create folder.")
